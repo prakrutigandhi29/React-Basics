a52cfeb562a90f607804b9c89de1ac6ebbdf4be5
Prakruti Gandhi <prakruti.gandhi@integrella.com>
2022-02-09 14:46:30 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Hello from './components/hello';
+import Class from './components/welcome';
+import JsxExample from './components/Jsx';
+import Message from './components/Message';
+import Function from './components/Function';
+import Parent from './components/Parent';
+import Condition from './components/condition';
+import List from './components/List';
+import Style from './components/stylesheet';
+import Form from './components/Form';
+import LifecycleA from './components/LifecycleA';
+import Table from './components/Fragmenttable';
+import ParentComp from './components/ParentComp';
+import Refs from './components/Refs';
+import RefsClassParent from './components/RefsClassParent';
+import Portals from './components/Portals';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {/* <Hello name='Prakruti'></Hello>
+      <Class></Class>
+      <JsxExample></JsxExample>
+      <Message></Message>
+      <Function/>
+  <Parent></Parent>
+  <Condition/>*/}
+  {/* <List/> */}
+  {/* <Style></Style> */}
+  {/* <Form/> */}
+  {/* <LifecycleA/> */}
+  {/* <Table/> */}
+  {/* <ParentComp/> */}
+  {/* <Refs/> */}
+  {/* <RefsClassParent/> */}
+  <Portals/>
     </div>
   );
 }
